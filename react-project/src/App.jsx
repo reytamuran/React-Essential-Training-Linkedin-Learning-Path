a8a9ad6d78a2ac7ac74b25e4f78e5525ba69e0e2
reytamuran <reytamuran@gmail.com>
2025-09-17 14:49:21 +0100

    02_03

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,8 +1,13 @@
 import './App.css'
 
+let language = "React";  //create variable
+let moon = "☽";
+
 function App() {
   return (
-   <h1>Hello World!</h1>
+   <h1>Hello {language.toUpperCase()} World! {moon}</h1> 
+   //variables can be injected into the body of the component using curly braces
+   // and methods works inside these braces
   )
 }
 
