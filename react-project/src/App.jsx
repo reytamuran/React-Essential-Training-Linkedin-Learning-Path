0d7e184d48d4a74a5c5b47374fae3b0368d160c6
reytamuran <reytamuran@gmail.com>
2025-09-17 15:28:17 +0100

    02_05

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
 
-function Header() {
+function Header(props) {  // we can reference those properties on the props 
+                          // object that is passed into each one of these values
 return (
   <header>
-    <h1>Eve's Kitchen </h1>
+    <h1>{props.name}'s Kitchen </h1>
+    <p>Copyright {props.year}</p>
   </header>
 )
 }
@@ -11,7 +13,8 @@ return (
 function App() {
   return (
     <div>
-      <Header/>
+       <Header name="Alex" year={new Date().getFullYear()} />  {/* when we render the component, 
+                                  we pass the properties into the component */}
       <main>
         <h2> We serve the most delicious food around</h2>
       </main>
